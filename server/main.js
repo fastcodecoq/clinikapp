7b7dab6a6c9504ebfc5778965f1657f02273484c
gomosoft <info@gomosoft.com>
2014-06-06 03:18:12 -0500

    ...

--- server/main.js
+++ server/main.js
@@ -1,1 +1,20 @@
 //la magia inicia aquí
+
+
+var express = require("express");
+var app = express();
+var bodyparser = require("body-parser");
+
+
+
+app.use(bodyparser());
+
+var router = express.Router();
+
+ router.get("/", function(req, res){
+ 	 res.json({error:false, message:"Bienvenid@ a nuestra API"});
+ });
+
+var puerto = process.env.PORT || 4000;
+
+app.listen()
