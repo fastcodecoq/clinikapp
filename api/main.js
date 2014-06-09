995f1d3ee7884a470026fe16e0bb96baa893b628
gomosoft <info@gomosoft.com>
2014-06-08 20:41:02 -0500

    usando todos los cores de la maquina para un mejor renidimiento

--- api/main.js
+++ api/main.js
@@ -3,11 +3,17 @@
 // ========================= requires ===============================
 
 
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyparser = require("body-parser");
-var mongoose = require("mongoose");
-var multer = require("multer");
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var multer = require('multer');
+var cluster = require('cluster');
+var cores = require('os').cpus().length;  //numero de cpus
+
+
+
+
 
 app.use(multer());
 app.use(bodyparser());  // usaremos JSON raw para envío de datos en modo
@@ -22,7 +28,7 @@ mongoose.connect('mongodb://clinikapp:clinikAPP.2014@localhost/clinikapp');  //n
 
 
 var router = express.Router();
-    router = require("./routes/base")(router);  //incluimos el archivo ./routes/base, llamamos a la función y le pasamos el router del app
+    router = require('./routes/base')(router);  //incluimos el archivo ./routes/base, llamamos a la función y le pasamos el router del app
 
 
 // ================= la route base contendrá todas las llamadas principales  ===========================
@@ -43,13 +49,34 @@ app.use(router);  //le pasamos las routes al app express
 
 
 
+
+
+// usamos todos los cores disponibles en la máquina
+// para un mejor rendimiento del app
+
+
+if (cluster.isMaster) {
+  
+  for (var i = 0; i < cores; i++)
+     cluster.fork();
+  
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+
+}
+else
+{
+
 // Instanciamiento del app 
 
 var pto = process.env.PORT || 8080;
 
 app.listen(pto);
+console.log('Magia en el puerto',pto);
 
-console.log("Magia en el puerto "+pto);
+}
 
 
 // ===========================================================
