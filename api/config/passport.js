c9fa72238e80621aa14748614d93c50152554985
Jairo Suárez <jasuarezpr@gmail.com>
2014-06-09 18:41:18 -0500

    arreglada la referencia a _id del sistema de logueo

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,6 +1,33 @@
 var LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth');
 var Credencial = require('../models/credenciales');
+var Logueo = require('../models/sistemasLogueo');
+
+var getSistemaDeLogueo = function(nombre, callback){
+  Logueo.findOne({nombre : nombre}, function(err,log){
+    if(err || log === null) {
+      // #TODO local solo deberia servir en desarrollo
+      if (nombre === 'local'){
+        logueo = new Logueo();
+        logueo.nombre = 'local';
+        logueo.key = 'local';
+        logueo.secret = 'local';
+        logueo.save(function(err){
+          if(err) callback(err);
+          else callback(null,logeo._id);
+        });
+      }else{
+        console.log({ message: 'no existe el sistema de logueo ' + nombre });
+        callback(err);
+      }
+    } else {
+      callback(null,log._id);
+    }
+  });
+};
+
+var existeCredencial = function(){
+};
 
 module.exports = function(passport) {
   passport.serializeUser(function(usuario,listo) {
@@ -15,21 +42,22 @@ module.exports = function(passport) {
     usernameField : 'email',
     passwordField : 'clave'
   },
-    function(email, clave, listo){
-      // TODO: checkear si el usuario existe
-      if(false){
-	return listo(null,false,'ya hay un usuario con ese correo');
-      } else {
-	var nuevaCredencial = new Credencial();
-	nuevaCredencial.email = email;
-	nuevaCredencial.token = clave;
-	nuevaCredencial.uid = email;
-	nuevaCredencial.save(function(err) {
-          console.log(email);
-          console.log(clave);
+  function(email, clave, listo){
+    // TODO: checkear si el usuario existe
+    if(false){
+      return listo(null,false,'ya hay un usuario con ese correo');
+    } else {
+      var nuevaCredencial = new Credencial();
+      nuevaCredencial.email = email;
+      nuevaCredencial.token = clave;
+      nuevaCredencial.uid = email;
+      getSistemaDeLogueo('local',function(err,log){
+        nuevaCredencial._id_sistema_logueo = log;
+        nuevaCredencial.save(function(err) {
           if (err) throw err;
           return listo(null, nuevaCredencial);
-	});
-      }
+        });
+      });
+    }
   }));
 };
