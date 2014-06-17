a1f8631f339bb55d7d55d97b440756a019321240
Jairo Suárez <jasuarezpr@gmail.com>
2014-06-17 10:29:33 -0500

    modificada lógica de tokens

--- api/arbiters/auth.js
+++ api/arbiters/auth.js
@@ -1,78 +1,55 @@
+var token = require('../helpers/token.js');
+var logger = require('../helpers/logs.js');
 
 var auth = {
- 
- estaLogueado :function (req, res, next){
-    
-    console.log(req.url)
-    
-    var logger = require('../helpers/logs.js');
-
-        logger(req, function(err, log){
-
-          console.log(log);
-
-          if (req.isAuthenticated())        
-          {
-
-           var usr = req.user;
-           var utils = require('../helpers/utils.js');
-
-           utils.validar_token(usr, function(err, credencial){
-
-              if(err){ 
-                 req.logout();
-                 return next(err);
-               }
-
-              next();
-
-           });
-
+  estaLogueado :function (req, res, next){
+    process.nextTick(function(){
+      logger(req, function(err, log){
+        if (req.isAuthenticated()) {
+          // Usuario autenticado usando passport
+          next();
         }
-        else if((req.query.token && req.query.uid) || (req.body.token && req.body.uid) || (req.params.token && req.params.uid)){
-           
-            var utils = require('../helpers/utils.js');   
-            var query = { uid : req.body.me || req.query.uid || req.body.uid || req.params.uid, token : req.query.token || req.body.token || req.params.token};
-            console.log(query);
-
-            utils.validar_token(query, function(err, credencial){
-
-                if(err) return res.json({error:true, message: 'no_autorizado'});
-
-                next();
-
-            });
-
+        else if(
+          // Usuario autenticado usando tokens
+          (req.query.token && req.query.uid) ||
+            (req.body.token && req.body.uid) ||
+              (req.params.token && req.params.uid)){
+          var query = { 
+            uid : req.body.me || req.query.uid || req.body.uid || req.params.uid,
+            token : req.query.token || req.body.token || req.params.token
+          };
+          utils.validar_token(query, function(err, credencial){
+            if(err)
+              return res.json({error:true, message: 'no_autorizado'});
+            next();
+          });
         }
         else
-        res.json({error:true, message: 'no_autorizado'});
-
-    });    
-         
-
-   }
-
-  ,
-
-  noLogueado : function (req, res, next){
-
-    var logger = require('../helpers/logs.js');    
-
-      logger(req, function(err, log){
-
-        console.log(log);
-
-         if (!req.isAuthenticated())   
-          return next();
-
-         res.redirect('/logueado');
+          return res.json({error:true, message: 'no_autorizado'});
 
       });    
+    });
+  },
+
+noLogueado : function (req, res, next){
+
+  var logger = require('../helpers/logs.js');    
+
+  logger(req, function(err, log){
+
+    console.log(log);
+
+    if (!req.isAuthenticated())   
+      return next();
+
+    res.redirect('/logueado');
+
+  });    
 
 
-   }
+}
 
- }
+}
 
 
 module.exports = auth;
