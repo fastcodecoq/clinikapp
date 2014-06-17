4b9cac60a5d48108ecb9d92cf778704c49cb3c4d
Jairo Suárez <jasuarezpr@gmail.com>
2014-06-17 10:30:30 -0500

    modificado login de google para nuevos models

--- api/config/passport.js
+++ api/config/passport.js
@@ -3,6 +3,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var liveStrategy = require('passport-windowslive').Strategy;
 var yahooStrategy = require('passport-yahoo-oauth').Strategy;
 var Credencial = require('../models/credenciales');
+var Token = require('../models/token');
 var Logueo = require('../models/sistemasLogueo');
 var credencialCtrl = require('../controllers/credencial');
 var passport = require('passport');
@@ -12,7 +13,7 @@ var bcrypt = require('bcrypt-nodejs');
 
 
 var getSistemaDeLogueo = function(nombre, callback){
-  
+
   Logueo.findOne({nombre : nombre}, function(err,log){
 
     if(err || log === null) {
@@ -41,307 +42,266 @@ passport.serializeUser(function(credencial,listo) {
   listo(null, credencial);
 });
 
- passport.deserializeUser(function(credencial,listo) {
-    listo(null, credencial);
+passport.deserializeUser(function(credencial,listo) {
+  listo(null, credencial);
 }); 
 
 
-// se elimino la parte de registro local, y se hace en el controlador registro
 
 
 passport.use('ingreso-local', new LocalStrategy({
-        usernameField : 'email',
-        passwordField : 'clave',
-        passReqToCallback : true
-    },
-    
-    function(req, email, clave, listo) {
+  usernameField : 'email',
+  passwordField : 'clave',
+  passReqToCallback : true
+},
+
+function(req, email, clave, listo) {
 
 
-      console.log(email);
+  console.log(email);
 
-      var validar = require('../helpers/validador');
+  var validar = require('../helpers/validador');
 
-      if(!validar.mail(email)) listo(null, false, 'params_invalidos');
-
-
-    Credencial.findOne({ 'email' :  email , 'sistema_logueo' : servicios.local.sistema_logueo },function(err, credencial) {
-
-        console.log(credencial)
-        
-        if (err)
-          return listo(err);
-
-        if(!credencial) 
-          return listo(null, false, 'credencial_invalida');
-        
-        if (!credencial)
-          return listo(null, false, 'correo_no_existe');
-
-       
-        if(! credencial.compararClaves(clave))
-          return listo(null, false, 'contraseña_incorrecta');
-
-        var utils = require('../helpers/utils');
-
-        credencial.token = utils.genToken( credencial._id + credencial.uid + credencial.token );
-        credencial.token_time = utils.strtotime('+1 hours');
-      
-
-        credencial.save(function(err, credencial){
-           
-           if(err) return listo(err, {});
-
-             console.log('hemos ingresado');
-             
-             var _credencial = {
-                'token' : credencial.token,
-                'uid' : credencial.email,
-                'email' : credencial.email,
-                'perfil_completado' : !!credencial.perfil_completado               
-             }; 
-
-             if(credencial.perfil_completado)
-             {
-
-              var Credencial = require('../models/credenciales');
-
-               Credencial.findOne(credencial).populate('_usuario').exec(function(err, credencial){
-
-            
-
-                   var _credencial = {
-                     'token' : credencial.token,
-                     'uid' : credencial.uid,
-                     'email' : credencial.email,
-                     'perfil_completado' : true,
-                     'usuario' : credencial._usuario 
-                   }; 
-
-                  console.log(_credencial)
+  if(!validar.mail(email)) listo(null, false, 'params_invalidos');
 
 
-                   return listo(null, _credencial);
+  Credencial.findOne({ 'email' :  email , 'sistema_logueo' : servicios.local.sistema_logueo },function(err, credencial) {
 
-                });
+    console.log(credencial)
 
-             }              
-            else                              
-                return listo(null, _credencial);
-               
+    if (err)
+      return listo(err);
 
-        });
-        
+    if(!credencial) 
+      return listo(null, false, 'credencial_invalida');
 
-        });
+    if (!credencial)
+      return listo(null, false, 'correo_no_existe');
 
 
-    }));
+    if(! credencial.compararClaves(clave))
+      return listo(null, false, 'contraseña_incorrecta');
+
+    var utils = require('../helpers/utils');
+
+    credencial.token = utils.genToken( credencial._id + credencial.uid + credencial.token );
+    credencial.token_time = utils.strtotime('+1 hours');
+
+
+    credencial.save(function(err, credencial){
+
+      if(err) return listo(err, {});
+
+      console.log('hemos ingresado');
+
+      var _credencial = {
+        'token' : credencial.token,
+        'uid' : credencial.email,
+        'email' : credencial.email,
+        'perfil_completado' : !!credencial.perfil_completado               
+      }; 
+
+      if(credencial.perfil_completado)
+        {
+
+          var Credencial = require('../models/credenciales');
+
+          Credencial.findOne(credencial).populate('_usuario').exec(function(err, credencial){
+
+
+
+            var _credencial = {
+              'token' : credencial.token,
+              'uid' : credencial.uid,
+              'email' : credencial.email,
+              'perfil_completado' : true,
+              'usuario' : credencial._usuario 
+            }; 
+
+            return listo(null, _credencial);
+
+          });
+
+        }              
+        else                              
+          return listo(null, _credencial);
+    });
+
+
+  });
+
+
+}));
 
 
 passport.use('google', new GoogleStrategy({    
-    clientID: servicios.google.id,
-    clientSecret: servicios.google.secret,
-    callbackURL: servicios.google.callbackURL
-  },
-  function(token, refreshToken, profile, listo) {
-
-    console.log(refreshToken);
-    
-    process.nextTick(function(){
-
-        console.log(token,profile);
-
-
-        var datos = {
-           'email' : profile._json.email,
-           'uid' : profile.id,
-           'token' : token,
-           'sistema_logueo' :  servicios.google.sistema_logueo,
-           'token_larga_vida' : true,
-           'token_autenticado' : true
-        };
-
-
-        console.log(datos)
-
-
-      credencialCtrl.existe({ email : datos.email}, function(err, exist){
-
-         if(err) return listo(err, null);
-
-         console.log(exist)
-
-          if(exist)
-            {
-              Credencial.findOne({uid : profile.id}, function(err, rs){
-                if(err)
-                  return listo(err, null);
-
-                if(rs)
-                {
-                rs.token = token;
-                rs.save(function(err,rs){
-                
-                console.log(rs);  
-                if(!err)
-                    return listo(err, rs); 
-                    
-                return listo(err, rs);
-
-                });
-                }
-                return listo(err, rs);
-
-
-                  
-              })
-            }
-          else
-            credencialCtrl.crear(datos, function(err, rs){
-
-                  return listo(err, rs);
+  clientID: servicios.google.id,
+  clientSecret: servicios.google.secret,
+  callbackURL: servicios.google.callbackURL
+},
+function(token, refreshToken, profile, listo) {
+  process.nextTick(function(){
+    credencialCtrl.existe({ email : profile._json.email}, function(err, exist){
+      if(err) return listo(err, null);
+      if(exist) {
+        Credencial.findOne({uid : profile.id}, function(err, rs){
+          if(err)
+            return listo(err, null);
+          if(rs) {
+            rs.auth.google = token;
+            rs.save(function(err,rs){
+              console.log(rs);  
+              if(!err)
+                return listo(err, rs); 
+              return listo(err, rs);
             });
-
-      });
-
-        
+          }
+          return listo(err, rs);
+        });
+      }
+      else {
+        var datos = {
+          'email' : profile._json.email,
+          'uid' : profile.id,
+          'sistema_logueo' :  servicios.google.sistema_logueo,
+        };
+        credencialCtrl.crear(datos, function(err, rs){
+          return listo(err, rs);
+        });
+      }
     });
-
-  }
-));
+  });
+}));
 
 
 
 passport.use('live', new liveStrategy({    
-    clientID: servicios.live.id,
-    clientSecret: servicios.live.secret,
-    callbackURL: servicios.live.callbackURL
-  },
-  function(token, refreshToken, profile, listo) {
+  clientID: servicios.live.id,
+  clientSecret: servicios.live.secret,
+  callbackURL: servicios.live.callbackURL
+},
+function(token, refreshToken, profile, listo) {
 
 
-    
-    process.nextTick(function(){
 
-        console.log(token,profile);
+  process.nextTick(function(){
 
-
-        var datos = {
-           'email' : profile._json.emails.preferred,
-           'uid' : profile.id,
-           'token' : token,
-           'sistema_logueo' : servicios.live.sistema_logueo
-        };
+    console.log(token,profile);
 
 
-     credencialCtrl.existe({email : datos.email}, function(err, exist){
+    var datos = {
+      'email' : profile._json.emails.preferred,
+      'uid' : profile.id,
+      'token' : token,
+      'sistema_logueo' : servicios.live.sistema_logueo
+    };
+
+
+    credencialCtrl.existe({email : datos.email}, function(err, exist){
 
       console.log('live')
 
 
-          if(exist)
-            {
-              Credencial.findOne({uid : profile.id}, function(err, credencial){
-                if(err)
-                  return listo(err, err);
+      if(exist)
+        {
+          Credencial.findOne({uid : profile.id}, function(err, credencial){
+            if(err)
+              return listo(err, err);
 
-                if(credencial)
-                {
+            if(credencial)
+              {
                 credencial.token = token;
                 credencial.save(function(err,credencial){
 
-                if(!err)
+                  if(!err)
                     return listo(err, credencial); 
-                    
-                return listo(err, credencial);
+
+                  return listo(err, credencial);
 
                 });
-                }
-                return listo(err, credencial);
+              }
+              return listo(err, credencial);
 
 
-                  
-              })
-            }
-          else
-            credencialCtrl.crear(datos, function(err, rs){
-                  profile['perfil_completado'] = false;
-                  return listo(err, rs);
-            });
 
-      });
+          })
+        }
+        else
+          credencialCtrl.crear(datos, function(err, rs){
+            profile['perfil_completado'] = false;
+            return listo(err, rs);
+          });
 
-        
     });
 
-  }
-));
+
+  });
+
+}));
 
 
 
 
 passport.use('yahoo', new yahooStrategy({    
-    consumerKey: servicios.yahoo.key,
-    consumerSecret: servicios.yahoo.secret,
-    callbackURL: servicios.yahoo.callbackURL
-  },
-  function(token, refreshToken, profile, listo) {
+  consumerKey: servicios.yahoo.key,
+  consumerSecret: servicios.yahoo.secret,
+  callbackURL: servicios.yahoo.callbackURL
+},
+function(token, refreshToken, profile, listo) {
 
 
-    
-    process.nextTick(function(){
 
-        console.log(token,profile);
+  process.nextTick(function(){
 
-
-        var datos = {
-           'email' : profile._json.emails.preferred,
-           'uid' : profile.id,
-           'token' : token,
-           'sistema_logueo' : servicios.yahoo.sistema_logueo
-        };
+    console.log(token,profile);
 
 
-      credencialCtrl.existe({email : datos.email}, function(err, exist){
+    var datos = {
+      'email' : profile._json.emails.preferred,
+      'uid' : profile.id,
+      'token' : token,
+      'sistema_logueo' : servicios.yahoo.sistema_logueo
+    };
+
+
+    credencialCtrl.existe({email : datos.email}, function(err, exist){
 
       console.log('yahoo')
 
 
-          if(exist)
-            {
-              Credencial.findOne({uid : profile.id}, function(err, credencial){
-                if(err)
-                  return listo(err, err);
+      if(exist)
+        {
+          Credencial.findOne({uid : profile.id}, function(err, credencial){
+            if(err)
+              return listo(err, err);
 
-                if(credencial)
-                {
+            if(credencial)
+              {
                 credencial.token = token;
                 credencial.save(function(err,credencial){
 
-                if(!err)
+                  if(!err)
                     return listo(err, credencial); 
-                    
-                return listo(err, credencial);
+
+                  return listo(err, credencial);
 
                 });
-                }
-                return listo(err, credencial);
+              }
+              return listo(err, credencial);
 
 
-                  
-              })
-            }
-          else
-            credencialCtrl.crear(datos, function(err, rs){
-                  profile['perfil_completado'] = false;
-                  return listo(err, rs);
-            });
 
-      });
+          })
+        }
+        else
+          credencialCtrl.crear(datos, function(err, rs){
+            profile['perfil_completado'] = false;
+            return listo(err, rs);
+          });
 
-        
     });
 
-  }
-));
+
+  });
+
+}));
